12fe51a1288069adb1d0377d655573b2d26c1d77
chaoqi <chaoqi@t.com>
2022-10-28 11:58:41 +0800

    测试2

---
+++ chapter6/13.add_word_plural.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+string count_num(string s);
+int main()
+{
+    char s[10] = {0};
+    cin >> s;
+    int len = strlen(s);
+    //cout << strlen(len) ;
+    return 0;
+    char temp;
+    // if (len > 0) {
+    //     temp = s[len-1];
+    //     if (temp == "y") {
+
+    //     } else {
+    //         cout << "乘机根为：" << s << endl;
+    //     }
+    // } else {
+    //     cout << "请输入单词" << endl;
+    // }
+    // cout << s.length();
+
+    return 0;
+}
